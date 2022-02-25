a915808ce55bc1003ba1d3570937c073b29e536f
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-02-25 14:28:44 -0600

    codico

--- Contadores/Contadores/Program.cs
+++ Contadores/Contadores/Program.cs
@@ -10,16 +10,16 @@ namespace Contadores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Agrege Numerico");
-            int numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriba el numero de iterciones que desea: ");
+            int valorFinal = int.Parse(Console.ReadLine());
            // int contador = 0;
            // int numero1 = 0;
-           // int contador2 = 0;
+           // int valor dinal = 0;
 
 
-            for (int contador = 0; contador <= numero1; contador++)
+            for (int contador = 1; contador <= valorFinal; contador++)
             {
-                Console.WriteLine("Ejecutando interacion numero " + contador);
+                Console.WriteLine("Escriba el numero " + contador);
             }
 
             Console.ReadKey();
